3f870aeea71108bd005dbcb7f1b871412fe1ee7a
Chris de Graaf <me@cdg.dev>
2020-06-23 17:07:23 -0500

    Fetch before checking out branch

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,12 @@
 
 set -euxo pipefail
 
+k="$(cat $SSH_KEY)"
+echo "$k" > "$SSH_KEY"
+chmod 400 "$SSH_KEY"
+git config core.sshCommand "ssh -o StrictHostKeyChecking=no -i $SSH_KEY"
+git remote add github "git@github.com:$GITHUB_REPOSITORY.git" || true
+git fetch github
 git checkout "$FROM"
 
 julia -e "
@@ -16,15 +22,10 @@ if [[ -z "$(git status -suno)" ]]; then
   exit 0
 fi
 
-k="$(cat $SSH_KEY)"
-echo "$k" > "$SSH_KEY"
-chmod 400 "$SSH_KEY"
-git config core.sshCommand "ssh -o StrictHostKeyChecking=no -i $SSH_KEY"
 git config user.name "github-actions[bot]"
 git config user.email "actions@github.com"
 git stash -u
 git checkout "$TO" 2> /dev/null || git checkout -b "$TO"
-git remote add github "git@github.com:$GITHUB_REPOSITORY.git"
 git pull github "$TO" || true
 git stash pop
 git commit -am "Rebuild $FOLDER/$FILE"
